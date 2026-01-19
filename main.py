73643f37f94db40802cd5a1f447191f97cba60e2
keesdekeijzer <git_kdk@xpmail.nl>
2026-01-19 20:46:39 +0100

    Voeg Notitie functionaliteit toe met bijbehorende UI en acties; verbeter de gebruikersinterface met nieuwe opties voor het invoegen van afbeeldingen en links in Markdown.

--- main.py
+++ main.py
@@ -14,11 +14,193 @@ import sqlite3
 
 # Pad naar de database : instelbaar maken?
 
+
 DATABASE = "/home/kees/Data/memo.db"
 
 DARKMODE = False
 
 OPSLAGLOCATIE = "/home/kees/Data/"
+
+class Notitie(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Memo")
+        self.setGeometry(100, 100, 600, 400)
+        loadUi("plaintext.ui",self)
+        self.setWindowTitle("Edith Notitie")
+
+        self.check_dark_mode()
+
+        self.actionNieuw.triggered.connect(self.nieuw)
+        self.actionOpslaan.triggered.connect(self.opslaan)
+        self.actionOpslaan_als.triggered.connect(self.opslaan_als)
+
+        self.actionOpen.triggered.connect(self.open)
+
+        self.actionSluiten.triggered.connect(self.sluiten)
+
+        self.actionKopieren.triggered.connect(self.kopieren)
+        self.actionKnippen.triggered.connect(self.knippen)
+        self.actionPlakken.triggered.connect(self.plakken)
+
+        self.actionZoeken.triggered.connect(self.zoeken)
+
+        self.actionUndo.triggered.connect(self.undo)
+        self.actionRedo.triggered.connect(self.redo)
+
+        self.actionAlles_selecteren.triggered.connect(self.alles_selecteren)
+
+        #self.actiondonkere_modus.triggered.connect(self.gebruik_donkere_modus)
+        #self.actionlichte_modus.triggered.connect(self.gebruik_lichte_modus)
+
+        #self.actionletters_groter.triggered.connect(self.letters_groter)
+        #self.actionletters_kleiner.triggered.connect(self.letters_kleiner)
+
+        self.actionDatum.triggered.connect(self.datum)
+        self.actionTijd.triggered.connect(self.tijd)
+        #self.actionVandaag.triggered.connect(self.vandaag)
+        self.actionmd_afbeelding.triggered.connect(self.md_afbeelding)
+        self.actionmd_link.triggered.connect(self.md_link)
+
+        self.actionOver_Edith.triggered.connect(self.over_edith)
+
+    def check_dark_mode(self):
+        dm = DARKMODE
+        print(f'donkere modus voor memo: {dm}')
+        if dm:
+            self.setStyleSheet('''
+                QWidget{
+                    background-color: rgb(33,33,33);
+                    color: #FFFFFF;
+                    }
+                QPlainTextEdit{
+                    background-color: rgb(46,46,46);
+                }
+                ''')
+
+    def nieuw(self):
+        print("nieuw")
+        self.plainTextEdit.clear()
+        self.setWindowTitle("Geen naam")
+        self.current_path = None   
+    
+    def opslaan(self):
+        print("opslaan")
+        if self.current_path is not None:
+            filetext = self.plainTextEdit.toPlainText()
+            print(filetext)
+            try:
+                with open(self.current_path, 'w') as f:
+                    f.write(filetext)
+                self.statusbar.showMessage("Bestand opgeslagen")
+            except Exception as e:
+                self.dialog_critical(str(e))
+        else:
+            self.opslaan_als()
+
+    def opslaan_als(self):
+        print("opslaan als")
+        try:
+            pathname = QFileDialog.getSaveFileName(self, 'Bestand opslaan', OPSLAGLOCATIE, 'Tekst bestanden (*.txt)')
+            print(pathname[0])
+            filetext = self.plainTextEdit.toPlainText()
+            with open(pathname[0], 'w') as f:
+                f.write(filetext)
+            self.current_path = pathname[0]
+            self.setWindowTitle(pathname[0])
+            self.statusbar.showMessage("Bestand opgeslagen")
+        except Exception as e:
+            self.dialog_critical(str(e))
+
+    def open(self):
+        print("open")
+        try:
+            fname = QFileDialog.getOpenFileName(self, 'Open bestand', OPSLAGLOCATIE, 'Tekst bestanden (*.txt);;Alle bestanden (*)')
+            print(fname[0]) # gekozen bestand
+            self.setWindowTitle(fname[0])
+            with open(fname[0], 'r') as f:
+                filetext = f.read()
+                self.plainTextEdit.setPlainText(filetext)
+            self.current_path = fname[0]
+            self.statusbar.showMessage("Bestand geopend")
+        except Exception as e:
+            self.dialog_critical(str(e))
+
+    def sluiten(self):
+        self.close()   
+
+    def kopieren(self):
+        self.plainTextEdit.copy()
+
+    def knippen(self):
+        self.plainTextEdit.cut()
+
+    def plakken(self):
+        self.plainTextEdit.paste()
+
+    def zoeken(self):
+        text, ok = QInputDialog.getText(self, 'Zoeken', 'Voer de zoekterm in:')
+        if ok and text:
+            gevonden = self.plainTextEdit.find(text)
+            if not gevonden:
+                # weer naar boven
+                cursor = self.plainTextEdit.textCursor()
+                cursor.movePosition(QTextCursor.MoveOperation.Start)
+                self.plainTextEdit.setTextCursor(cursor)
+                gevonden = self.plainTextEdit.find(text)
+                print(f"'{text}' niet gevonden")
+                if not gevonden:
+                    term = text.replace('<','&lt;')
+                    term = term.replace('>','&gt;')
+                    QMessageBox.information(self, "Vinden", f"'{term}' niet gevonden")
+
+    def undo(self):
+        self.plainTextEdit.undo()
+
+    def redo(self):
+        self.plainTextEdit.redo()
+
+    def alles_selecteren(self):
+        self.plainTextEdit.selectAll()
+
+    def datum(self): # todo
+        nu = datetime.datetime.now()
+        datum_nu = nu.strftime("%Y-%m-%d")
+        print(f"datum: {datum_nu}")
+        self.plainTextEdit.insertPlainText(datum_nu)
+
+    def tijd(self):
+        nu = datetime.datetime.now()
+        tijd_nu = nu.strftime("%H:%M")
+        print(f"tijd: {tijd_nu}")
+        self.plainTextEdit.insertPlainText(tijd_nu)
+
+    def md_afbeelding(self):
+        print("md afbeelding")
+        pathname = QFileDialog.getOpenFileName(self, 'Afbeelding openen', OPSLAGLOCATIE, 'Afbeeldingen (*.png *.jpg *.jpeg *.bmp *.gif);;Alle bestanden (*)')
+        print(pathname[0]) # gekozen bestand
+        if pathname[0]:
+            bestandsnaam = pathname[0].split('/')[-1]
+            md_code = f"![{bestandsnaam}]({pathname[0]})"
+            self.plainTextEdit.insertPlainText(md_code)
+
+    def md_link(self):
+        print("md link")
+        pathname = QFileDialog.getOpenFileName(self, 'Bestand openen', OPSLAGLOCATIE, 'Alle bestanden (*)')
+        print(pathname[0]) # gekozen bestand
+        if pathname[0]:
+            bestandsnaam = pathname[0].split('/')[-1]
+            md_code = f"[{bestandsnaam}]({pathname[0]})"
+            self.plainTextEdit.insertPlainText(md_code)
+
+    def over_edith(self):
+        QMessageBox.about(self, "Over Edith", "Edith versie 1.0\nEen eenvoudige teksteditor met memo functionaliteit.")
+
+    def dialog_critical(self, s):
+        dlg = QMessageBox(self)
+        dlg.setText(s)
+        dlg.setIcon(QMessageBox.Icon.Critical)
+        dlg.show()
 
 class MemoLijst(QMainWindow):
     def __init__(self):
@@ -398,6 +580,8 @@ class Venster(QMainWindow):
 
         self.actionNormaliseren.triggered.connect(self.normaliseren)
 
+        self.actionPlatte_Tekst.triggered.connect(self.platteTekst)
+
         self.actiondonkere_modus.triggered.connect(self.gebruik_donkere_modus)
         self.actionlichte_modus.triggered.connect(self.gebruik_lichte_modus)
 
@@ -421,6 +605,7 @@ class Venster(QMainWindow):
 
         self.actionOpen_memo.triggered.connect(self.open_memo)
         self.actionMemo_lijst.triggered.connect(self.memo_lijst)
+        self.actionNotitie.triggered.connect(self.notitie)
 
         self.actionOver_Edith.triggered.connect(self.over_edith)
 
@@ -753,6 +938,16 @@ class Venster(QMainWindow):
     def over_edith(self):
         print("over edith")
         QMessageBox.about(self, "Over Edith", "Edith is een tekstbewerker gemaakt met Python en PyQt6")
+
+    def platteTekst(self):
+        print("platte tekst")
+        plain_text = self.textEdit.toPlainText()
+        self.textEdit.setPlainText(plain_text)
+
+    def notitie(self):
+        print("notitie")
+        self.notitie_venster = Notitie()
+        self.notitie_venster.show()
 
     def dialog_critical(self, s):
         dlg = QMessageBox(self)
