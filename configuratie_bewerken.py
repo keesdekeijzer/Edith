fac3266191e1fac4a6a39cf2a2877759bcea2a3d
keesdekeijzer <git_kdk@xpmail.nl>
2026-06-10 20:08:04 +0200

    Enhance configuration editing: add support for blue mode and update UI with save functionality

--- configuratie_bewerken.py
+++ configuratie_bewerken.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QDialog, QLabel, QVBoxLayout
+from PyQt6.QtWidgets import QDialog, QLabel, QMessageBox, QRadioButton, QVBoxLayout, QPushButton as QButton
 import yaml
 
 class ConfiguratieBewerken(QDialog):
@@ -14,8 +14,27 @@ class ConfiguratieBewerken(QDialog):
         layout = QVBoxLayout()
         label = QLabel("Hier kun je de configuratie bewerken.")
         layout.addWidget(label)
-        label2 = QLabel("Deze functionaliteit is nog in ontwikkeling.")
+        label2 = QLabel("Light mode / Dark mode / Blue mode")
         layout.addWidget(label2)
+        self.mode_choice1 = QRadioButton("Light mode")
+        self.mode_choice2 = QRadioButton("Dark mode")
+        self.mode_choice3 = QRadioButton("Blue mode")
+        layout.addWidget(self.mode_choice1)
+        layout.addWidget(self.mode_choice2)
+        layout.addWidget(self.mode_choice3)
+
+        config = self.load_config()
+        if config.get('darkmode') == 'light':
+            self.mode_choice1.setChecked(True)
+        elif config.get('darkmode') == 'dark':
+            self.mode_choice2.setChecked(True)
+        else:
+            self.mode_choice3.setChecked(True)
+
+        mode_btn = QButton("Opslaan")
+        layout.addWidget(mode_btn)  
+        mode_btn.clicked.connect(self.bevestig_mode)
+
         self.setLayout(layout)
 
     def load_config(self):
@@ -36,4 +55,19 @@ class ConfiguratieBewerken(QDialog):
         config['darkmode'] = 'light'
         self.save_config(config)
 
-    
+    def bevestig_mode(self):
+        print("Mode opgeslagen!")
+        config = self.load_config()
+        if self.mode_choice1.isChecked():
+            config['darkmode'] = 'light'
+        elif self.mode_choice2.isChecked():
+            config['darkmode'] = 'dark'
+        else:
+            config['darkmode'] = 'blue'
+        self.save_config(config)
+        self.melding_opgeslagen()
+
+        
+    def melding_opgeslagen(self):
+        print("Configuratie opgeslagen!")
+        QMessageBox.information(self, "Opgeslagen", "Configuratie is opgeslagen!")
